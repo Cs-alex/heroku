f28b19c133f816b06b6885454b91dfbf4def3566
Cs-alex <alex.csende@gmail.com>
2017-09-06 21:34:44 +0200

    update

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Game</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <style>
             .box {
                 width: 100px;
