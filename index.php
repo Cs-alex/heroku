58752e641d1317db8523ee20f6055ff915482a5b
Cs-alex <alex.csende@gmail.com>
2018-09-18 22:27:20 +0200

    xy

--- index.php
+++ index.php
@@ -240,7 +240,7 @@
                 $(window).on('scroll', function() {
                     [].slice.call(parallax).forEach(function(el, i){
                         var windowYOffset = window.pageYOffset,
-                        elBackgrounPos = "right " + (windowYOffset * speed) + "px";
+                        elBackgrounPos = "right " + (windowYOffset * speed + 8) + "px";
                         el.style.backgroundPosition = elBackgrounPos;
                     });
                     if (progress == false) {
